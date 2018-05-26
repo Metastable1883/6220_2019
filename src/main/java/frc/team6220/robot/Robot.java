a46f7cd300a35c1eaa0e49ed7a00df56324b9e26
Metastable1883 <boson2003@gmail.com>
2018-05-26 14:45:20 -0700

    added initialization

--- src/main/java/frc/team6220/robot/Robot.java
+++ src/main/java/frc/team6220/robot/Robot.java
@@ -1,6 +1,7 @@
 package frc.team6220.robot;
 
-import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 /**
  * @author Owen Ryan
@@ -9,9 +10,19 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  * TODO code stuff
  */
 public class Robot extends IterativeRobot {
+    private DifferentialDrive drive;
+    private Encoder driveL;
+    private Encoder driveR;
+    private Joystick Joysitck1;
+    private Joystick Joystick2;
+
+
     @Override
     public void robotInit() { }
-
+    Talon exampleTalon0 = new Talon(1);
+    Talon exampleTalon1 = new Talon(2);
+    Talon exampleTalon2 = new Talon(3);
+    Talon exampleTalon3 = new Talon(4);
     @Override
     public void disabledInit() { }
 
